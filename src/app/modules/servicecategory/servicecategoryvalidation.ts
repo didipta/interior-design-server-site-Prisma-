4fe1f224bce1e91ffb511b23ea8a323514ee2d7f
didipta <66532706+didipta@users.noreply.github.com>
2023-10-14 16:34:42 +0600

    update

--- src/app/modules/servicecategory/servicecategoryvalidation.ts
+++ src/app/modules/servicecategory/servicecategoryvalidation.ts
@@ -1,12 +1,3 @@
-// model servicecategory{
-//   id        String   @id @default(uuid())
-//   categoryname String
-//   service service[]
-//   createdAt  DateTime @default(now())
-//   updatedAt  DateTime @updatedAt
-//   @@map("servicecategory")
-// }
-
 import { z } from 'zod';
 
 export const category = z.object({
